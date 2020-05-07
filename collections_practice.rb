b8c2980daa923af0022e95f3e9fa871b15718abf
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-05-07 16:21:30 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -15,6 +15,11 @@ def swap_elements_from_to(array, index, destination_index)
   return array
 end
 
+def swap_elements_from_to(array, index, destination_index)
+  array[index], array[destination_index] = array[destination_index], array[index]
+  return array
+end
+
 def reverse_array(integers)
   new_array = integers.reverse
   new_array
